9f70d34f0bcd1591b78a63079c94e3aa98912cde
Nagababu23 <nagababuakula413@gmail.com>
2025-01-02 11:51:31 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "../css/home.css"
 
-class Home extends Component{
-    render(){
-        return(
-            <div>
-                <h1>hii</h1>
+class Home extends Component {
+  render(){
+    return (
+        <div>
+            <div className='homecard'>
+                <h1>Professional Cleaning Services</h1>
+                <h4>Experience the difference with our expert cleaning solutions</h4>
+                <button>Book Now</button>
             </div>
-        )
-    }
+        </div>
+      );
+  }
 }
 export default Home
